19ee1d4ac970dd4b250d871ee4686a809134bc22
xcdev <xdev.git@gmail.com>
2026-01-06 19:26:29 +0900

    rediscluster service 객체 정의

---
+++ internal/k8sutils/cluster/type_service.go
@@ -0,0 +1,226 @@
+package cluster
+
+import (
+	"context"
+	"strconv"
+
+	rcvb2 "github.com/xcdev-0/redis-operator/api/v1beta2"
+	"github.com/xcdev-0/redis-operator/internal/k8sutils/consts"
+	"github.com/xcdev-0/redis-operator/internal/k8sutils/k8smeta"
+	"github.com/xcdev-0/redis-operator/internal/k8sutils/services"
+	"github.com/xcdev-0/redis-operator/internal/util"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/utils/ptr"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+)
+
+// RedisClusterService는 Redis Cluster Service 생성을 위한 파라미터를 담는 구조체입니다.
+type RedisClusterService struct {
+	role string // Service 역할 ("leader" 또는 "follower")
+}
+
+func (rcs RedisClusterService) CreateRedisClusterService(ctx context.Context, cr *rcvb2.RedisCluster, cl kubernetes.Interface) error {
+	serviceName := cr.Name + "-" + rcs.role
+	var epp k8smeta.ExporterPortProvider
+	if cr.Spec.RedisExporter != nil {
+		epp = func() (port int, enable bool) {
+			defaultP := ptr.To(consts.RedisExporterPort)
+			return *util.Coalesce(cr.Spec.RedisExporter.Port, defaultP), cr.Spec.RedisExporter.Enabled
+		}
+	} else {
+		epp = k8smeta.DisableMetrics
+	}
+
+	// 1. cluster ip 타입 서비스
+	labels := k8smeta.GetRedisLabels(&k8smeta.RedisLabels{
+		Name:      serviceName,
+		SetupType: k8smeta.Cluster,
+		Role:      rcs.role,
+		Labels:    cr.Labels,
+	})
+
+	objectMeta := k8smeta.GenerateObjectMeta(&k8smeta.ObjectMeta{
+		Name:        serviceName,
+		Namespace:   cr.Namespace,
+		Labels:      labels,
+		Annotations: k8smeta.GenerateServiceAnots(cr.ObjectMeta, nil, epp),
+	})
+
+	busPort := corev1.ServicePort{
+		Name:     consts.RedisBusPortName,
+		Port:     int32(*cr.Spec.ClientPort + 10000), // Service 포트
+		Protocol: corev1.ProtocolTCP,
+		TargetPort: intstr.IntOrString{
+			Type:   intstr.Int,
+			IntVal: int32(*cr.Spec.ClientPort + 10000), // Pod의 컨테이너 포트
+		},
+	}
+
+	extraPorts := []corev1.ServicePort{}
+	if cr.Spec.KubernetesConfig.ShouldIncludeBusPort() {
+		extraPorts = append(extraPorts, busPort)
+	}
+
+	var err error
+	err = services.CreateOrUpdateService(ctx, services.ServiceOptions{
+		Namespace:            cr.Namespace,
+		ServiceObjectMeta:    objectMeta,
+		OwnerRef:             k8smeta.RedisClusterAsOwner(cr),
+		ExporterPortProvider: epp,
+		Headless:             false,
+		ServiceType:          "ClusterIP",
+		ClientPort:           *cr.Spec.ClientPort,
+		K8sClient:            cl,
+		ExtraPorts:           extraPorts,
+	})
+	if err != nil {
+		log.FromContext(ctx).Error(err, "Cannot create service for Redis", "Setup.Type", rcs.role)
+		return err
+	}
+
+	// 2. headless service
+	headlessObjectMeta := k8smeta.GenerateObjectMeta(&k8smeta.ObjectMeta{
+		Name:        serviceName + "-headless",
+		Namespace:   cr.Namespace,
+		Labels:      labels,
+		Annotations: k8smeta.GenerateServiceAnots(cr.ObjectMeta, nil, epp),
+	})
+	headlessExtraPorts := []corev1.ServicePort{}
+	if cr.Spec.KubernetesConfig.ShouldIncludeBusPortForHeadless() {
+		headlessExtraPorts = append(headlessExtraPorts, busPort)
+	}
+	// Headless Service 생성 (ClusterIP 타입, headless=true)
+	err = services.CreateOrUpdateService(ctx, services.ServiceOptions{
+		Namespace:            cr.Namespace,
+		ServiceObjectMeta:    headlessObjectMeta,
+		OwnerRef:             k8smeta.RedisClusterAsOwner(cr),
+		ExporterPortProvider: k8smeta.DisableMetrics,
+		Headless:             true,
+		ServiceType:          "ClusterIP",
+		ClientPort:           *cr.Spec.ClientPort,
+		K8sClient:            cl,
+		ExtraPorts:           headlessExtraPorts,
+	})
+	if err != nil {
+		log.FromContext(ctx).Error(err, "Cannot create headless service for Redis", "Setup.Type", rcs.role)
+		return err
+	}
+
+	// 3. additional service
+	additionalObjectMeta := k8smeta.GenerateObjectMeta(&k8smeta.ObjectMeta{
+		Name:        serviceName + "-additional",
+		Namespace:   cr.Namespace,
+		Labels:      labels,
+		Annotations: k8smeta.GenerateServiceAnots(cr.ObjectMeta, cr.Spec.KubernetesConfig.GetServiceAnnotations(), epp),
+	})
+	additionalExtraPorts := []corev1.ServicePort{}
+	if cr.Spec.KubernetesConfig.ShouldIncludeBusPortForAdditional() {
+		additionalExtraPorts = append(additionalExtraPorts, busPort)
+	}
+	if cr.Spec.KubernetesConfig.ShouldCreateAdditionalService() {
+		err = services.CreateOrUpdateService(ctx, services.ServiceOptions{
+			Namespace:            cr.Namespace,
+			ServiceObjectMeta:    additionalObjectMeta,
+			OwnerRef:             k8smeta.RedisClusterAsOwner(cr),
+			ExporterPortProvider: k8smeta.DisableMetrics,
+			Headless:             false,
+			ServiceType:          cr.Spec.KubernetesConfig.GetServiceType(),
+			ClientPort:           *cr.Spec.ClientPort,
+			K8sClient:            cl,
+			ExtraPorts:           additionalExtraPorts,
+		})
+		if err != nil {
+			log.FromContext(ctx).Error(err, "Cannot create additional service for Redis", "Setup.Type", rcs.role)
+			return err
+		}
+	}
+	// 4. master service
+	masterObjectMeta := k8smeta.GenerateObjectMeta(&k8smeta.ObjectMeta{
+		Name:        cr.Name + "-master",
+		Namespace:   cr.Namespace,
+		Labels:      labels,
+		Annotations: k8smeta.GenerateServiceAnots(cr.ObjectMeta, nil, epp),
+	})
+	err = services.CreateOrUpdateService(ctx, services.ServiceOptions{
+		Namespace:            cr.Namespace,
+		ServiceObjectMeta:    masterObjectMeta,
+		OwnerRef:             k8smeta.RedisClusterAsOwner(cr),
+		ExporterPortProvider: k8smeta.DisableMetrics,
+		Headless:             false,
+		ServiceType:          "ClusterIP",
+		ClientPort:           *cr.Spec.ClientPort,
+		K8sClient:            cl,
+		ExtraPorts:           []corev1.ServicePort{},
+	})
+	if err != nil {
+		log.FromContext(ctx).Error(err, "Cannot create master service for Redis", "Setup.Type", rcs.role)
+		return err
+	}
+
+	// 5. nodeport type 일경우 추가로 생성
+	if cr.Spec.KubernetesConfig.GetServiceType() == "NodePort" {
+		err = rcs.createOrUpdateClusterNodePortService(ctx, cr, cl)
+		if err != nil {
+			log.FromContext(ctx).Error(err, "Cannot create nodeport service for Redis", "Setup.Type", rcs.role)
+			return err
+		}
+	}
+	return nil
+}
+
+func (rcs RedisClusterService) createOrUpdateClusterNodePortService(ctx context.Context, cr *rcvb2.RedisCluster, cl kubernetes.Interface) error {
+	replicaCount := cr.Spec.GetReplicaCounts(rcs.role)
+
+	// 각 Pod마다 개별 NodePort Service 생성
+	for i := 0; i < int(replicaCount); i++ {
+		// 예: redisutils-cluster-leader-0, redisutils-cluster-leader-1, ...
+		serviceName := cr.Name + "-" + rcs.role + "-" + strconv.Itoa(i)
+		serviceLabels := k8smeta.GetRedisLabels(
+			&k8smeta.RedisLabels{
+				Name:      cr.Name + "-" + rcs.role,
+				SetupType: k8smeta.Cluster,
+				Role:      rcs.role,
+				Labels: map[string]string{
+					// sts가 팟에게 자동으로 붙이는 레이블, 특정 팟만 선택하기 위해 사용
+					"statefulset.kubernetes.io/pod-name": serviceName,
+				},
+			},
+		)
+		serviceAnnotations := k8smeta.GenerateServiceAnots(cr.ObjectMeta, nil, k8smeta.DisableMetrics)
+		serviceObjectMeta := k8smeta.GenerateObjectMeta(&k8smeta.ObjectMeta{
+			Name:        serviceName,
+			Namespace:   cr.Namespace,
+			Labels:      serviceLabels,
+			Annotations: serviceAnnotations,
+		})
+
+		// Redis Cluster Bus 포트 정의
+		busPort := corev1.ServicePort{
+			Name:     consts.RedisBusPortName,
+			Port:     int32(*cr.Spec.ClientPort + 10000), // Service 포트
+			Protocol: corev1.ProtocolTCP,
+			TargetPort: intstr.IntOrString{
+				Type:   intstr.Int,
+				IntVal: int32(*cr.Spec.ClientPort + 10000), // Pod의 컨테이너 포트
+			},
+		}
+		// NodePort Service 생성 (각 Pod마다 고유한 NodePort 할당)
+		err := services.CreateOrUpdateService(ctx, services.ServiceOptions{
+			Namespace:            cr.Namespace,
+			ServiceObjectMeta:    serviceObjectMeta,
+			OwnerRef:             k8smeta.RedisClusterAsOwner(cr),
+			ExporterPortProvider: k8smeta.DisableMetrics,
+			Headless:             false,
+			ServiceType:          "NodePort",
+			ClientPort:           *cr.Spec.ClientPort,
+			ExtraPorts:           []corev1.ServicePort{busPort},
+		})
+		if err != nil {
+			log.FromContext(ctx).Error(err, "Cannot create nodeport service for Redis", "Setup.Type", rcs.role)
+			return err
+		}
+	}
+	return nil
+}
